2e748a41f993d32baefe58d9415ab22f372b2291
Carlos-Madrid-Jimenez <carlosmadridjimenez2005@gmail.com>
2025-03-18 11:37:04 +0100

    Siguiendo el tutorial

--- addons2/estate/__manifest__.py
+++ addons2/estate/__manifest__.py
@@ -1,14 +1,18 @@
 {
     'name': "Estate",
     'version': '17.0.1.2',
-    'depends': ['base'],
+    'depends': [],
     'author': "Carlos Madrid Jiménez",
     'category': 'Category',
     'description': """
     Description text
     """,
     # data files always loaded at installation
-    'data': [],
+    'data': [
+        'data/security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
     # data files containing optionally loaded demonstration data
     'demo': [],
 }
