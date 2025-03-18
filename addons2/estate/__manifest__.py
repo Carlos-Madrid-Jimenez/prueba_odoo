193170af0f4d5979dada66abd49d3ecff4157956
Carlos-Madrid-Jimenez <carlosmadridjimenez2005@gmail.com>
2025-03-18 10:30:18 +0100

    A

---
+++ addons2/estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Estate",
+    'version': '17.0.1.2',
+    'depends': ['base'],
+    'author': "Carlos Madrid Jiménez",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [],
+    # data files containing optionally loaded demonstration data
+    'demo': [],
+}
